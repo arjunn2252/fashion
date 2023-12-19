24ee0a65bd18c02ee05f7404d7a479ca0a73f3f0
Arjun Thakur <arjuanthakur@Arjuns-MacBook-Pro.local>
2023-12-20 01:04:16 +0530

    Initial commit

---
+++ src/main/java/com/fashion/pair/AllInOneFashionStopForAPersonApplication.java
@@ -0,0 +1,15 @@
+package com.fashion.pair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllInOneFashionStopForAPersonApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!!");
+		SpringApplication.run(AllInOneFashionStopForAPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fashion/pair/AllInOneFashionStopForAPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fashion.pair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllInOneFashionStopForAPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
